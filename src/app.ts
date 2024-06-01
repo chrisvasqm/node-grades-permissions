f32da42d648d48848f4c61f073b812f31a4976ec
chrisvasqm <christianvasquezmelo@gmail.com>
2024-06-01 07:32:14 -0400

    Setup the Express app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("Hello world");
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/', (_, res) => {
+    res.send('Welcome to the Grades API');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
